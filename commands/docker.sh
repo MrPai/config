237da8033a7cff8caf9a766d11f45f2e9b6af752
MrPai <1164934857@qq.com>
2022-08-19 11:57:52 +0800

    add parachain-launch

--- commands/docker.sh
+++ commands/docker.sh
@@ -1,12 +1,18 @@
 ##  基于docker container修改容器并生成新image
 // https://www.jb51.net/article/200508.htm
 
-# 启动容器
+### 启动容器
 docker run -it image_id /bin/sh
-# 拷贝宿主机文件到容器
+### 拷贝宿主机文件到容器
 docker cp /opt/test/file.txt mycontainer-id:/opt/testnew/
-# 使用修改后的容器新建镜像
+### 使用修改后的容器新建镜像
 docker commit -m "描述内容" -a "author name" 32555789dd00 aipaper/devinz83:v2
-# 检查镜像
+### 检查镜像
 docker images
+
+
+## 加入当前用户到docker组
+### 直接使用docker命令而不是 `sudo docker`
+sudo gpasswd -a $USER docker 
+newgrp docker
 
---
+++ commands/parachain-launch.sh
@@ -0,0 +1,20 @@
+## 升级旧版本(一个命令就够了)
+npm update -g @open-web3/parachain-launch
+
+
+whereis parachain-launch
+# /home/ubuntu/.nvm/versions/node/v14.17.0/bin/parachain-launch
+# 注意区分npm安装的global package目录与yarn安装的 global package目录
+# 注意分别添加目录到环境变量中，然后source ~/.bash_profile
+
+# 卸载
+npm uninstall -g @open-web3/parachain-launch
+
+# 通过yarn的方式安装
+yarn global add @open-web3/parachain-launch
+
+
+# 通过 yarn安装的在 yarn的bin目录下
+# 注意与node下的bin目录区分
+export PATH="$PATH:$(yarn global bin)"
+yarn global dir
